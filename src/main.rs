f3d5d683b817bb92c024050f87754fdb24c298d9
ivana-19 <i.cosic.2@campus.tu-berlin.de>
2024-06-28 14:39:32 +0200

    Aufabe1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    //"https://github.com/Pirate-Weather/pirateweather"
+    let response = reqwest::get("https://api.pirateweather.net/forecast/5CNI3wn3PL2L6wIia7aX9RqMdyIA4ARJ/52.520008,13.404954").await?;
+
+    if response.status().is_success() {
+        let body = response.text().await?;
+        println!("Response: {}", body);
+    } else {
+        println!("Failed to get a successful response: {}", response.status());
+    }
+
+    Ok(())
 }
